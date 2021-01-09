d8ca97a3c5aa2c55b8e5a70509c6dce131609699
q2419068625 <2419068625@qq.com>
2021-01-09 22:51:58 +0800

    请求数据并渲染

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home container">
+   <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">姓名</th>
+      <th scope="col">电话</th>
+      <th scope="col">邮箱</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="item in customers" :key="item.id">
+      <th scope="row">{{item.id}}</th>
+      <td>{{item.name}}</td>
+      <td>{{item.phone}}</td>
+      <td>{{item.email}}</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { defineComponent, onMounted, reactive, ref, toRefs } from 'vue';
+import axios from 'axios';
 export default defineComponent({
   name: 'Home',
+  setup() {
+    const state = reactive({
+      customers: []
+    })
+
+    //发起请求
+    onMounted(async () => {
+      const res = await axios.get("http://localhost:3000/users")
+      console.log(res.data);
+      state.customers = res.data
+    })
+
+    return {...toRefs(state)}
+  },
   components: {
-    HelloWorld,
   },
 });
 </script>
