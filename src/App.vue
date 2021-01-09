8e9fae84de361625ece65467e69f5c0a58cdbb0c
q2419068625 <2419068625@qq.com>
2021-01-09 22:17:43 +0800

    添加导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <router-link class="navbar-brand" to="/">用户管理系统</router-link>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">主页</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">关于我们</router-link>
+      </li>
+      
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/add">添加用户</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+   
   </div>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
